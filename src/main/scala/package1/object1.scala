7dbb71dac0faaf79cfe7c87dbd9a45d2d4128eab
kkreddyk <kkreddy.k@gmail.com>
2022-07-16 17:43:26 +0100

    first commit from eclipse

--- src/main/scala/package1/object1.scala
+++ src/main/scala/package1/object1.scala
@@ -1,10 +1,17 @@
 package package1
 
+import org.apache.spark.sql.SparkSession
+
 object object1 {
   
   
   def main(args: Array[String])
   {
     println("----")
+    
+    var spark=SparkSession.builder.master("local[1]").getOrCreate()
+    
+    spark.sql("show databases").show()
+    
   }
 }
